083647cd558e93629a647630f37c140669765adf
Eugene Ivantsov <eivantsov@redhat.com>
2017-11-27 08:41:43 +0200

    ide-server extension

---
+++ plugins/plugin-serverservice/plugin-serverservice-ide/src/main/java/org/eclipse/che/sample/ide/MyServiceClient.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2012-2017
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.che.sample.ide;
+
+import javax.inject.Inject;
+import org.eclipse.che.api.promises.client.Promise;
+import org.eclipse.che.ide.api.app.AppContext;
+import org.eclipse.che.ide.rest.AsyncRequestFactory;
+import org.eclipse.che.ide.rest.StringUnmarshaller;
+import org.eclipse.che.ide.ui.loaders.request.LoaderFactory;
+
+/**
+ * Client for consuming the sample server service.
+ *
+ * @author Edgar Mueller
+ */
+public class MyServiceClient {
+
+  private AppContext appContext;
+  private AsyncRequestFactory asyncRequestFactory;
+  private LoaderFactory loaderFactory;
+
+  /**
+   * Constructor.
+   *
+   * @param appContext the {@link AppContext}
+   * @param asyncRequestFactory the {@link AsyncRequestFactory} that is used to create requests
+   * @param loaderFactory the {@link LoaderFactory} for displaying a message while waiting for a
+   *     response
+   */
+  @Inject
+  public MyServiceClient(
+      final AppContext appContext,
+      final AsyncRequestFactory asyncRequestFactory,
+      final LoaderFactory loaderFactory) {
+
+    this.appContext = appContext;
+    this.asyncRequestFactory = asyncRequestFactory;
+    this.loaderFactory = loaderFactory;
+  }
+
+  /**
+   * Invoke the sample server service.
+   *
+   * @param name a parameter
+   * @return a Promise containing the server response
+   */
+  public Promise<String> getHello(String name) {
+    return asyncRequestFactory
+        .createGetRequest(appContext.getMasterApiEndpoint() + "/hello/" + name)
+        .loader(loaderFactory.newLoader("Waiting for hello..."))
+        .send(new StringUnmarshaller());
+  }
+}
---
+++ plugins/plugin-serverservice/plugin-serverservice-server/src/main/java/org/eclipse/che/sample/inject/ServerServiceGuiceModule.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2012-2017
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.che.sample.inject;
+
+import com.google.inject.AbstractModule;
+import org.eclipse.che.inject.DynaModule;
+import org.eclipse.che.sample.MyService;
+
+/** Server service example Guice module for setting up a simple service. */
+@DynaModule
+public class ServerServiceGuiceModule extends AbstractModule {
+
+  @Override
+  protected void configure() {
+    bind(MyService.class);
+  }
+}
